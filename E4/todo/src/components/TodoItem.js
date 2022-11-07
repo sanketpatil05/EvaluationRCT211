af6314effab6f2487ffd302c0c0b516c054d05d3
sanketpatil05 <patilsanket209@gmail.com>
2022-11-07 12:01:51 +0530

    added todo

--- E4/todo/src/components/TodoItem.js
+++ E4/todo/src/components/TodoItem.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-const TodoItem = ({ item, deleteItem, editItem }) => {
+const TodoItem = ({ item, edit_func, delete_func}) => {
   const [completed, setCompleted] = React.useState(item.completed);
   const [edit, setEdit] = React.useState(false);
   const [editData, setEditData] = React.useState(item.title);
 
-  const handleEdit = () => {
-    editItem(item.id, editData);
-    setEdit(false);
-  };
+  
   return (
     <>
       <tr>
@@ -20,13 +17,13 @@ const TodoItem = ({ item, deleteItem, editItem }) => {
           <td>{item.title} </td>
         )}
         <td>
-          <button onClick={() => deleteItem(item.id)}>delete</button>
+          <button onClick={()=>{delete_func(item.id)}} >delete</button>
         </td>
         <td>
-          <button onClick={() => setCompleted(!completed)}>Completed</button>
+          <button >Completed</button>
         </td>
         <td>
-          <button onClick={() => setEdit(true)}>Edit</button>
+          <button onClick={()=>setEdit(true)}>Edit</button>
           {edit && (
             <>
               <input
@@ -34,7 +31,7 @@ const TodoItem = ({ item, deleteItem, editItem }) => {
                 value={editData}
                 onChange={(e) => setEditData(e.target.value)}
               />
-              <button onClick={handleEdit}>Done</button>
+              <button onClick={()=>{edit_func(item.id,editData )}} >Done</button>
             </>
           )}
         </td>
