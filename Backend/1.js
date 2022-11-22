da8d676285de9f698917ad23d885a5f3bdd54330
sanketpatil05 <patilsanket209@gmail.com>
2022-11-22 18:47:25 +0530

    added backend

---
+++ Backend/1.js
@@ -0,0 +1,3 @@
+const os= require("os");
+console.log(os.version()) // To get the version of of your os
+console.log(os.cpus()[0])    // To get info about your cpus. You can access cpu depending on how much core you have present 
