6a21718d4d6ef0570a390a154292a4131b3ad3ca
sanketpatil05 <patilsanket209@gmail.com>
2022-11-23 13:47:02 +0530

    added

--- Backend/1.js
+++ Backend/1.js
@@ -1,3 +1,13 @@
 const os= require("os");
 console.log(os.version()) // To get the version of of your os
 console.log(os.cpus()[0])    // To get info about your cpus. You can access cpu depending on how much core you have present 
+
+http : Hyper text transfer protocol :-> use for to coummunicate between client and server 
+ftp : file transfer protocol 
+
+1st time :-> 3 way handshake
+client :- sends a signal I want to communicate acknowledge(means say hi) the signal /request <-- server 
+client :- communicating --> I want so and so 
+
+http +s :- secure--> add more secrutiy 
+SSL :-> secure scoket layer 
