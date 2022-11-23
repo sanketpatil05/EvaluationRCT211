50bb1783042d94132a102ee2a0a3c98998818dda
sanketpatil05 <patilsanket209@gmail.com>
2022-11-23 14:07:07 +0530

    added https notes

--- Backend/1.js
+++ Backend/1.js
@@ -11,3 +11,32 @@ client :- communicating --> I want so and so
 
 http +s :- secure--> add more secrutiy 
 SSL :-> secure scoket layer 
+
+
+
+bandwidth - pipe -> 2cm 1/4 liter
+            pipe -> 15cm 1 liter
+            it is like how much data it can be transfer at perticular point 
+
+MBps !=Mbps 
+b- bits 
+B-bytes 
+
+1 byte = 8 bits 
+
+
+data packets /data chunks  data comes in this 
+chunks get created by automatically by the bandwidth
+chunks out response in our code 
+
+
+http verbs / http methods  --> 
+GET --> To get data from server 
+POST --> posting/sending/adding to the server 
+PUT --> it replace whole data or add whole new things (like removing the old tyre and replacing with new one)
+PATCH --> for patch the data can changed only what who want (like take out puncher from tyre)
+DELETE --> delete the particular data 
+
+
+
+Headers --> gives more/ additional information about the request/response 
