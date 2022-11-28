09df4b4fde9d8ae5e2e23e96d32b6c1a004ced47
sanketpatil05 <patilsanket209@gmail.com>
2022-11-28 21:38:52 +0530

    added

--- Backend/Middleware/5.js
+++ Backend/Middleware/5.js
@@ -1,1 +1,4 @@
-// mongodb is no sql data base used for storing the data 
+// mongodb is no sql data base used for storing the data and making CRUD operations 
+// mongodb stores the data in bson format , means it is in binary form. 
+// Videos and imgaes are stored in other sotrage and link to that is sotred in mongodb
+
