620112d77dc6a140fcb9193abae4b8049c32bb42
sanketpatil05 <patilsanket209@gmail.com>
2022-11-29 22:53:05 +0530

    mongodb

--- Backend/Middleware/5.js
+++ Backend/Middleware/5.js
@@ -2,3 +2,6 @@
 // mongodb stores the data in bson format , means it is in binary form. 
 // Videos and imgaes are stored in other sotrage and link to that is sotred in mongodb
 
+
+// mongod  queries 
+// find().pretty()
