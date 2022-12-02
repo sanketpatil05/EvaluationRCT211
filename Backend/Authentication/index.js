6cd1ffecefbec78f8e8bc58e5bddcac5dbed5ab6
sanketpatil05 <patilsanket209@gmail.com>
2022-12-02 23:27:02 +0530

    added db.js

--- Backend/Authentication/index.js
+++ Backend/Authentication/index.js
@@ -1,1 +1,19 @@
-const x=3; 
+const express= require("express");
+const {connect}= require("./db")
+const app=express();
+app.use(express.json())
+
+
+app.get("/", (req, res)=>{
+    res.send("You are on hompage")
+})
+app.listen(8000, async()=>{
+    try {
+           await connect 
+           console.log("conntect to db")
+    }
+    catch (err){
+        console.log(err);
+    }
+    console.log("Server is running at 8000")
+})
