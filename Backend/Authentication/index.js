ff8be1985e2d9f9bc91fa4fc29e31ce860770d49
sanketpatil05 <patilsanket209@gmail.com>
2022-12-06 11:01:45 +0530

    added

--- Backend/Authentication/index.js
+++ Backend/Authentication/index.js
@@ -5,19 +5,27 @@ app.use(express.json())
 
 
 app.get("/", (req, res)=>{
-    res.send("You are on hompage")
+    res.send("You are on hompage and this ishjjgjh homepage ")
 })
 
 app.get("/about", (req, res)=>{
-    res.send("You are on about")
+    res.send("You are on about and  and aovlhkjbut page")
 })
+
+app.get("/contact", (req, res)=>{
+res.send("You are on contact")
+})
+
+
 app.listen(8000, async()=>{
-    try {
-           await connect 
-           console.log("conntect to db")
-    }
-    catch (err){
-        console.log(err);
-    }
-    console.log("Server is running at 8000")
+    // try {
+    //        await connect 
+    //        console.log("conntect to db")
+    // }
+    // catch (err){
+    //     console.log(err);
+    // }
+
+
+    console.log("Server is running at port of  8000")
 })
