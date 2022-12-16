aab731fe36d34f06f4ef8e2497f42d720a65f41a
sanketpatil05 <patilsanket209@gmail.com>
2022-12-16 23:54:39 +0530

    added

--- Backend/Authentication/index.js
+++ Backend/Authentication/index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 
 
 app.get("/", (req, res)=>{
-    res.send("You are on and is  afdafdasdfdfasdnfadsfsdfadste aksdf;l homepage ")
+    res.send("You are on and is  afdafdaladnfadfdfasdnfadsfsdfadste aksdf;l homepage ")
 })
 
 app.get("/student", async(req, res)=>{
